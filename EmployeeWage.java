032e2f6827d619c3f1757b629f24e42ff0f5903d
kalevishal <kalevishal1610@gmail.com>
2021-09-18 21:45:55 +0530

    Added Calculating Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,13 +1,20 @@
-public class Attendance {
-    
+public class DailyEmployeeWage {
+
     public static void main(String[] args) {
-         //Constants
-        
-        double empCheck = Math.floor(Math.random()* 10)%2;
-         //Computation
-        if(empCheck == 1 )
-        System.out.println("Employee is present");
+        //Constants
+        int FULL_TIME = 1;
+        int PER_HR = 20;
+        //Variables
+        int empHr = 0;
+        int empWage = 0;
+        //Computation
+        double empCheck = Math.floor(Math.random()*10)%2;
+        if (empCheck == FULL_TIME)
+            empHr = 8;
         else
-            System.out.println("Employee is not present");
+            empHr = 0;
+        empWage = empHr * PER_HR ;
+        System.out.println("employee Wage: " + empWage);
+
     }
- }
+}
