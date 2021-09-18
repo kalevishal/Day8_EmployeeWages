bf331e598a284b5407560acde03568bba5c68fbc
kalevishal <kalevishal1610@gmail.com>
2021-09-18 21:50:45 +0530

    Adding Part Time Employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,20 +1,22 @@
-public class DailyEmployeeWage {
+public class AddPartTime {
 
     public static void main(String[] args) {
         //Constants
         int FULL_TIME = 1;
+        int PART_TIME = 2;
         int PER_HR = 20;
-        //Variables
-        int empHr = 0;
-        int empWage = 0;
-        //Computation
-        double empCheck = Math.floor(Math.random()*10)%2;
-        if (empCheck == FULL_TIME)
-            empHr = 8;
+         ///Variables
+        int emphr = 0;
+        int empday = 0;
+         //Computation
+        double empcheck = Math.floor(Math.random()*10)%3;
+        if (empcheck == PART_TIME)
+            emphr = 4;
+        else if (empcheck == FULL_TIME)
+            emphr = 8;
         else
-            empHr = 0;
-        empWage = empHr * PER_HR ;
-        System.out.println("employee Wage: " + empWage);
-
+            emphr = 0;
+        empday = emphr * PER_HR ;
+        System.out.println("employee wage : " +empday);
     }
 }
