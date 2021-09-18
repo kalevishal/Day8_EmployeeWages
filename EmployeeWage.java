a6d4c4e734d9850f75be6f6b6fbe55e6d61c5f73
kalevishal <kalevishal1610@gmail.com>
2021-09-18 22:00:00 +0530

    Added: Calculating wage for month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,29 +1,39 @@
-public class UsingSwitchCase {
+public class WageForMonth {
 
-        public static final int PART_TIME = 1;
-        public static final int Full_TIME = 2;
-        public static final  int PER_HR = 20;
+    public static final int PART_TIME = 1;
+    public static final int FULL_TIME = 2;
+    public static final int PER_HR = 20;
+    public static final int NO_OF_WORKING_DAY = 2;
 
     public static void main(String[] args) {
-         //Variables
+        //Variables
         int emphr = 0;
         int empwage = 0;
-         //Computation
-        int empcheck =(int) Math.floor(Math.random()*10)%3;
-        switch (empcheck) {
-            case PART_TIME:
-                emphr = 4;
-                break;
-            case Full_TIME:
-                emphr = 8;
-                break;
-            default:
-                emphr = 0;
-               
+        int totalwage = 0;
+          //Computation
+          for (int day = 0; day < NO_OF_WORKING_DAY; day++) {
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+                switch (empCheck) {
+                    case PART_TIME:
+                        emphr = 4;
+                        break;
+                    case FULL_TIME:
+                        emphr = 8;
+                        break;
+                    default:
+                        emphr = 0;
+
+                }
+                empwage = emphr * PER_HR;
+                totalwage += empwage;
+                System.out.println("Emp Wage: " + empwage);
+            }
+        System.out.println("Total Emp Wgae: " + totalwage);
+
+
         }
-        empwage = emphr * PER_HR;
-        System.out.println("employee wage: " +empwage);
+
+
+
 
     }
-
-}
