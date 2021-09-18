786cfb4217abce41e6999a0f787295b89850fbec
kalevishal <kalevishal1610@gmail.com>
2021-09-18 22:04:51 +0530

    Added: Calculating Wage working day total working hour and month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,39 +1,35 @@
-public class WageForMonth {
-
-    public static final int PART_TIME = 1;
+public class HourDayPerMonth {
+    public static final int PART_time = 1;
     public static final int FULL_TIME = 2;
     public static final int PER_HR = 20;
     public static final int NO_OF_WORKING_DAY = 2;
+    public static final int MAX_N0_OF_MONTH = 10;
 
     public static void main(String[] args) {
-        //Variables
+        //Variable
         int emphr = 0;
-        int empwage = 0;
-        int totalwage = 0;
-          //Computation
-          for (int day = 0; day < NO_OF_WORKING_DAY; day++) {
-                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                switch (empCheck) {
-                    case PART_TIME:
-                        emphr = 4;
-                        break;
-                    case FULL_TIME:
-                        emphr = 8;
-                        break;
-                    default:
-                        emphr = 0;
-
-                }
-                empwage = emphr * PER_HR;
-                totalwage += empwage;
-                System.out.println("Emp Wage: " + empwage);
+        int totalemphr = 0;
+        int totalworkingday = 0;
+         //computation
+        while (totalemphr <= MAX_N0_OF_MONTH && totalworkingday < NO_OF_WORKING_DAY ){
+            totalworkingday++;
+            int empCheck = (int)  Math.floor(Math.random()*10)%3;
+            switch (empCheck){
+                case PART_time:
+                    emphr = 4;
+                    break;
+                case FULL_TIME:
+                    emphr = 8;
+                    break;
+                default:
+                    emphr = 0;
+                    break;
             }
-        System.out.println("Total Emp Wgae: " + totalwage);
-
+            totalworkingday +=emphr;
+            System.out.println("day: " +totalworkingday + "hr: " +emphr);
 
         }
-
-
-
-
-    }
+         int totalempwork = totalworkingday * PER_HR;
+        System.out.println("employee wage: " +totalempwork);
+   }
+}
